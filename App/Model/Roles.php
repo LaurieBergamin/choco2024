a8e51f2663bd6bdeb06c3e3b6bd28d6e7d5d2319
laurie.bergamin <laurie.bergamin@gmail.com>
2024-01-10 10:32:29 +0100

    model Utilisateur

---
+++ App/Model/Roles.php
@@ -0,0 +1,20 @@
+<?php 
+namespace App\Model;
+use App\Utils\BddConnect;
+class Roles extends BddConnect{
+
+    private ?int $id_roles;
+    private ?string $nom_roles;
+    public function getId(): ?int{
+        return $this->id_roles;
+    }
+    public function setId(int $id): void{
+        $this->id_roles = $id;
+    }
+    public function getNom(): ?string{
+        return $this->nom_roles;
+    }
+    public function setNom(string $nom): void{
+        $this->nom_roles = $nom;
+    }
+}
